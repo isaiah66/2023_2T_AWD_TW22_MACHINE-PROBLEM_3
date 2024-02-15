0f7142cc22ec4e90fb7743b9ee20632934f0ec8c
isaiah66 <156283424+isaiah66@users.noreply.github.com>
2024-02-15 23:46:57 +0800

    everyday.js

--- everyday/everyday.js
+++ everyday/everyday.js
@@ -1,21 +1,25 @@
 function moveBox(color) {
     var box1 = document.getElementById('box1');
     var box2 = document.getElementById('box2');
-    
+  
     box1.style.transition = 'top 1.5s ease-in-out, background-color 2.5s ease-out';
     box2.style.transition = 'top 1.5s ease-in-out, background-color 2.5s ease-out';
-    
+    box1.style.backgroundColor = color;
+
+
+    box2.style.setProperty('--border-color', color);
+  
     if (box1.style.top === '300px') {
-        box1.style.top = '-300px';
-        box2.style.setProperty('--border-color', color);
-        box2.style.backgroundColor = color;
-        box2.style.top = '300px';
-        box1.style.backgroundColor = 'white';
+      box1.style.top = '-300px';
+      box2.style.setProperty('--border-color', color);
+      box2.style.backgroundColor = color;
+      box2.style.top = '300px';
+      box1.style.backgroundColor = 'white';
     } else {
-        box1.style.top = '300px';
-        box1.style.setProperty('--border-color', color);
-        box1.style.backgroundColor = color;
-        box2.style.top = '-900px';
-        box2.style.backgroundColor = 'white';
+      box1.style.top = '300px';
+      box1.style.setProperty('--border-color', color);
+      box1.style.backgroundColor = color;
+      box2.style.top = '-900px';
+      box2.style.backgroundColor = 'white';
     }
 }
